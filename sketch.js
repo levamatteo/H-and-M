a83b2b0dbbe63814ee6c54dbbca74d7f16b16413
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-09-24 16:24:09 -0700

    new update

---
+++ sketch.js
@@ -0,0 +1,288 @@
+
+
+let roster = [
+
+ {
+
+firstName: "Lydia",
+
+lastName: "Lim",
+
+Color: "blue",
+
+Animal : "dog",
+
+book: "No",
+
+movie: "me before you",
+
+game: "No",
+
+superhero: "Thor",
+
+randomFact: " birds are tetrapods even though they only walk on two legs",
+};
+
+
+{
+
+firstName:"Zainib",
+
+lastName:"Ahmed",
+
+color:"Blue",
+
+animal:"Giraffe",
+
+book:"The Book Thief",
+
+movie:"Amelie",
+
+game:"none",
+
+superhero:"The Hulk",
+
+randomFact:"I love to eat olives."};
+
+{
+
+  firstName: "Toni",
+
+  lastName: "Anguiano",
+
+
+  Color: "blue",
+
+  Animal: "cat",
+
+    Book: "The Bell Jar",
+
+    MovieGenre: "Horror",
+
+    SuperHero: "batman",
+
+    randomFact: "I like to hangout in cemeteries and research dark California history",
+};
+
+{
+
+      firstName: "Camila",
+
+     lastName: "Avina Garcia",
+
+     Color: "Green",
+
+     Animal:"Dog",
+
+     Book:"Match",
+
+    Movie:"Lady and the Tramp",
+
+    Game:" Guitar Hero",
+
+    Superhero:"Spiderman",
+
+    randomFact:"I have a twin",
+
+}
+
+{
+
+firstName: "Matteo",
+
+lastName: "Leva",
+
+color: "Black",
+
+animal: "Cat",
+
+book: "Pillars of the earth,
+
+movie:"The Godfather",
+
+game:  "Witcher 3",
+
+superHero: "spider man",
+
+fact:"I'm disappointed in GOT season 8",
+}
+
+{
+
+firstName:"Eddson",
+
+lastName:"Jose",
+
+color:"midnight blue",
+
+animal:"pig",
+
+book:"The Dark Forest",
+
+movie:"The Fountain",
+
+game:"Halo Reach",
+
+superhero:"Superman",
+
+fact:"I am made up of stardust"
+};
+
+{
+
+firstName: "Hector",
+
+lastName: "Flores",
+
+color: "Blue",
+
+animal:"Dog",
+
+book:"Captain Underpants",
+
+movie:"Ace Ventura pet detective",
+
+game:"Smash Brothers",
+
+superhero:"Flash",
+
+randomFact:"I am a Libra", }
+
+{
+firstName: "Lydia",
+lastName: "Lim",
+Color: "blue",
+Animal : "dog",
+book: "No",
+movie: "me before you",
+game: "No",
+superhero: "Thor",
+randomFact: " birds are tetrapods even though they only walk on two legs",
+};
+
+{firstName:"Chi",
+lastName:"Nguyen",
+Color:"purple",
+animal:"bird",
+book:"Math Curse",
+movie:"Big Hero 6",
+game:"Checkers",
+superhero(es):"Big Hero 6 (team)",
+randomFact:"Lately this year, monsters have especially been occupying my mind, among other things."};
+
+{
+firstName: "Leon",
+lastName: "Quach",
+Color: "silver",
+animal:"penguins",
+book: "Letters to a Young Poet",
+movie:"Kung fu Hustle",
+game: "Pokemon Crystal Version",
+superhero: "Batman",
+randomFact:"I have two pet guinea pigs"
+};
+
+{
+
+firstName: "Sierra"
+
+lastName: "Roe"
+
+.color = purple
+
+.animal = dog
+
+.book = phantom tollbooth
+
+.movie = Naussica of the Valley of the Wind
+
+.game = pokemon heartgold
+
+.superhero = captain marvel
+
+randomFact: can run 7 minute mile
+
+};
+
+{
+firstName: "Hao",
+lastName: "Rong",
+Color: hex(#4285F4),
+animal: "otter",
+book: "A Brief History of Time",
+movie: "Angry Birds 2",
+game: "Final Fantasy XIV",
+superhero: "Tony Stark",
+randomFact: "I like cats.",
+};
+
+{
+    firstname: "Kaicong",
+    lastname: "Tang",
+    favColor: "black",
+    animal: "dog",
+    movie: "iron man123",
+    game: "csgo",
+    superhero: "iron man",
+    fact: "love gaming",
+};
+
+{
+firstName: "Sumedh",
+lastName: "Vedanthi",
+faveColor:"Blue" ,
+animal: "Crow",
+book: "Of Mice and Men".
+movie: "American Psycho",
+game: "Fruit Ninja" ,
+superHero: "Batman" ,
+randomFact: "im a black belt",
+};
+
+{
+firstName: "Chris",
+lastName: "Weber",
+Color: "green",
+Animal: "cat",
+Book: "Hitchhiker's Guide to the Galaxy",
+Movie: "Spiderman Into The Spiderverse",
+Game: "The Legend of Zelda Breath of the Wild",
+SuperHero: "Iron Man",
+randomFact: "I'm vegetarian"
+};
+
+{
+firstName: "Andy",
+lastName: "Wu",
+Color: "Red",
+animal:"Kiwi",
+book: "Romance of the Three Kingdoms",
+movie:"Shrek 2",
+game: "Super Mario 64",
+superhero: "Static Shock",
+randomFact:"I've had jaw correction surgery"
+};
+
+{
+
+firstName: "xiaowei",
+
+lastName: "zheng",
+
+Color:" green",
+
+Animal: "babies",
+
+Book: "the little girl at the window",
+
+Movie: "eat drink man woman",
+
+Game: none,
+
+SuperHero: none,
+
+randomFact: "don't drink coke"
+
+};
+
+]
